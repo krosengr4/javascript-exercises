611151afb04e4768b9080ce1cc46c9e157070daf
Kevin Rosengren <rosenkev4@gmail.com>
2026-05-14 22:26:48 -0500

    Completed loop and array exercises.
    These exercises focussed on the foundations of loops and arrays in
    JavaScript.
    07_reverseString
    08_removeFromArray
    09_sumAll
    10_leapYears
    11_tempConversion

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    const result = [];
+
+    for (const item of arr) {
+        if (!args.includes(item)) {
+            result.push(item);
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+// LEAP YEARS
+// Divisible by 4
+// NOT divisible by 100 UNLESS divisible by 400
+// Years ending in 00 MUST BE divisible by 400 to be leap years
+const leapYears = function (year) {
+    const divisibleByFour = year % 4 == 0;
+    const isCentury = year % 100 == 0;
+    const divisibleByFourHundred = year % 400 == 0;
 
+    if (divisibleByFour && (!isCentury || divisibleByFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
