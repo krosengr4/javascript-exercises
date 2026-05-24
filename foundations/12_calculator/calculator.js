82feea9e0965f0d54dee7747d22139cf8dc306cd
Kevin Rosengren <rosenkev4@gmail.com>
2026-05-24 16:09:39 -0500

    Completed exercise working with operations and array methods in JavaScript.
    The goal for this exercise is to create a calculator that does the
    following:
    
    1. add(): take two numbers and return their sum
    2. subtract(): take two numbers and return the result after subtracting
    the second from the first
    3. sum(): take an array of numbers and add all the numbers together
    4. multiply(): take an array of numbers and multiply all the numbers
    together
    5. power(): take two numbers (base and exponent) and return the result
    of raising the base to that exponent
    6. factorial(): given a single number, find its factorial

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+    return nums.reduce((total, currentVal) => total + currentVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+    return nums.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+    return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    if (num === 0 || num === 1) {
+        return 1;
+    }
+
+    // Recursive step: call this function again but with 1 less than current number
+    return num * factorial(num - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
