9ab1ee219c814b5eb18a63045b943c7197012ab4
Kevin Rosengren <rosenkev4@gmail.com>
2026-06-03 20:51:10 -0500

    Completed palindrome javascript problem.
    Write a function that determines whether or not a given string is a
    palindrome.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const chars = str.split("");
+    const sortedChars = [];
 
+    for (const char of chars) {
+        if (
+            char != " " &&
+            char != "," &&
+            char != "." &&
+            char != "?" &&
+            char != "!"
+        ) {
+            sortedChars.push(char);
+        }
+    }
+
+    const sortedString = sortedChars.join("");
+    const reverseString = sortedChars.reverse().join("");
+
+    if (sortedString.toLowerCase() === reverseString.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
