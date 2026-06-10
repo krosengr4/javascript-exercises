f942b5147073a2dbe94ab99b6defb4b8ab528103
Kevin Rosengren <rosenkev4@gmail.com>
2026-06-10 01:53:54 -0500

    Completed getOldestPerson exercise/.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    const currentYear = new Date().getFullYear();
 
+    if (!death) {
+        death = currentYear;
+    }
+    return death - birth;
+}
+
+const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(
+            currentPerson.yearOfBirth,
+            currentPerson.yearOfDeath,
+        );
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
