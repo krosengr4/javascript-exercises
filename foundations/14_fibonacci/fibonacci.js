65508e45cd57993069ae0204aee8020baddd603d
Kevin Rosengren <rosenkev4@gmail.com>
2026-06-09 21:12:31 -0500

    Completed fibonacci sequence exercise.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (countArg) {
+    let count;
+    if (typeof countArg !== "number") {
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
 
+    if (count < 0) {
+        return "OOPS";
+    }
+    if (count == 0) {
+        return 0;
+    }
+
+    let firstPrev = 1;
+    let secPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secPrev;
+        secPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
